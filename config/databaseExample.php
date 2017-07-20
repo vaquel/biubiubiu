f7ace373526357bf9a35508b7bbb5d20e3c84279
陈龙 <503526664@qq.com>
2017-07-20 15:26:36 +0800

    fix

---
+++ config/databaseExample.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'pgsql',
+    'host' => 'localhost',
+    'database' => '',
+    'username' => '',
+    'password' => '',
+    'charset' => 'utf8',
+    'collation' => 'utf8_general_ci',
+    'prefix' => ''
+];
